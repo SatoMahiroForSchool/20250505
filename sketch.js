a4a5fec8f82553ed32a55f923d58b3b020bfc0cf
SatoMahiroForSchool <409731139@gms.tku.edu.tw>
2025-05-05 10:40:27 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,38 +5,35 @@ let skeleton;
 
 function setup() {
   createCanvas(640, 480);
-	//createCanvas(windowWidth, windowHeight);
   video = createCapture(VIDEO);
   video.hide();
-  poseNet = ml5.poseNet(video, modelLoaded); //呼叫在ml5.js上的net函數，用此函數來判斷各位置，呼叫成功即執行function modelLoaded 
+  poseNet = ml5.poseNet(video, modelLoaded); 
   poseNet.on('pose', gotPoses);
 }
 
 function gotPoses(poses) {
-  console.log(poses); // 檢查輸出
   if (poses.length > 0) {
-    pose = poses[0].pose;  //把抓到的幾個點，都放置pose變數內
-    skeleton = poses[0].skeleton; //把相關於骨架的點都放到skeleton變數內
+    pose = poses[0].pose;  
+    skeleton = poses[0].skeleton; 
   }
 }
 
-
-function modelLoaded() {   //顯示pose model已經準備就緒
+function modelLoaded() {   
   console.log('poseNet ready');
 }
 
 function draw() {
   background(0);
-  image(video, 0, 0);
-  translate(video.width, 0);
-  scale(-1, 1);
+  image(video, 0, 0);  
+  translate(video.width, 0);  
+  scale(-1, 1);    
 
   if (pose) {
     // 繪製關節點
     for (let i = 0; i < pose.keypoints.length; i++) {
       let keypoint = pose.keypoints[i];
-      if (keypoint.score > 0.2) { // 確保檢測點的置信度足夠高
-        fill(255, 0, 0);
+      if (keypoint.score > 0.2) { // 確保置信度足夠高
+        fill(0, 255, 0);
         noStroke();
         ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
       }
@@ -46,10 +43,8 @@ function draw() {
     for (let i = 0; i < skeleton.length; i++) {
       let partA = skeleton[i][0];
       let partB = skeleton[i][1];
-      stroke(255, 255, 0);
+      stroke(255, 0, 0);
       line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
-      translate(video.width,0)  //因為攝影機顯示的是反像的畫面，需要透過這兩條指令來做反轉
-	    scale(-1,1)    //因為攝影機顯示的是反像的畫面，需要透過這兩條指令來做反轉
     }
   }
 }
